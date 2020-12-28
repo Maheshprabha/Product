192ed53a4a6d05df53ef9152c749dc43fda5e6bc
admin <admin@DESKTOP-ATOQJFI>
2020-12-28 09:22:11 -0800

    konakartproduct

---
+++ Konakarts/src/main/java/com/atmecs/konakart/constants/FilePaths.java
@@ -0,0 +1,12 @@
+package com.atmecs.konakart.constants;
+
+public class FilePaths {
+
+
+	public static final String CHROME_FILE = "./lib/chromedriver.exe";
+	public static final String CONFIG_FILE = "./src/test/resources/config.properties";
+	public static final String LOG4J_FILE = "./src/test/resources/log4j.properties";
+	public static final String HOMEPAGE_LOCFILE = "./src/test/resources/locators/homepage.properties";
+	public static final String TESTDATA_FILE = "./src/test/resources/testdata/testdata.xlsx";
+	public static final String VALIDDATA_FILE= "./src/test/resources/validadata/validationdata.properties";
+}
